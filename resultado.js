dddfba5379e0f5e03a99dd4e3dc99af858881099
Ariel-peralta-A <arielperalta123456789@gmail.com>
2026-06-15 21:57:35 -0300

    formulario

---
+++ resultado.js
@@ -0,0 +1,14 @@
+const parametros =
+new URLSearchParams(window.location.search);
+
+const nombre = parametros.get("nombre");
+const email = parametros.get("email");
+const edad = parametros.get("edad");
+const pais = parametros.get("pais");
+
+document.getElementById("datos").innerHTML = `
+<p><strong>Nombre:</strong> ${nombre}</p>
+<p><strong>Email:</strong> ${email}</p>
+<p><strong>Edad:</strong> ${edad}</p>
+<p><strong>País:</strong> ${pais}</p>
+`;
